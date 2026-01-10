b65cdf6c292b3ad69c3dcb7b3af9cc113cee74a7
RoyGeova07 <royumana3@gmail.com>
2026-01-09 23:38:48 -0600

    Aprendiendo del uso de las cookies en la web

--- Autenticacion_de_usuarios/config.ts
+++ Autenticacion_de_usuarios/config.ts
@@ -2,6 +2,7 @@ export const
 {
 
     PORT=3000,
-    SALT_ROUNDS=10
+    SALT_ROUNDS=10,
+    SECRET_JWT_KEY='this-is-an-awesome-secret-key-mucho-mas-largo-y-muy-seguro'
 
 }=process.env
--- Autenticacion_de_usuarios/index.ts
+++ Autenticacion_de_usuarios/index.ts
@@ -1,18 +1,52 @@
 import express,{Request,Response}from 'express'
-import {PORT}from './config'
+import {PORT, SECRET_JWT_KEY}from './config'
 import { UserRepository } from './user-repository';
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'//me sirve para modificar las cookies 
 
 const app=express()
 
 //middleware: es una funciona q se ejecuta antes de que llegue a las peticiones 
 app.set('view engine','ejs')
+
+/*
+estos son los middlewares: son funciones por las que pasa la peticion, osea modificamos,
+ya puede ser la peticion o la respuesta y dejamos q pase la peticion a la siguiente funcion a la que le toca
+*/
 app.use(express.json())
+app.use(cookieParser())
 
 //ENDPOINTS
 app.get('/',(_req:Request,res:Response)=>
 {
 
-    res.render('index')
+    const token=_req.cookies.acceso_token
+
+    if(!token)
+    {
+
+        return res.render('index')//no logueado
+
+    }
+
+    try
+    {
+
+        const datos=jwt.verify(token,SECRET_JWT_KEY)
+        if(typeof datos==='string')
+        {
+
+            return res.render('index')//token raro
+
+        }
+        return res.render('index',{username: datos.username})
+
+
+    }catch(error){
+
+        return res.render('index')//token invalido/expirado
+
+    }
 
 });
 
@@ -25,7 +59,19 @@ app.post('/login',async(req,res)=>
     {
 
         const user=await UserRepository.login({username,password})
-        res.send({user})
+        const token=jwt.sign({id: user._id,username: user.username},
+            SECRET_JWT_KEY,
+        {
+            expiresIn:'1h'
+        })
+        res
+            .cookie('acceso_token',token,{
+                httpOnly: true,//la cookie solo se puede acceder en el servidor
+                secure: process.env.NODE_ENV==='production',//la cookie solo se puede acceder en https
+                sameSite: 'strict',//la cookie solo se puede acceder en el mismo dominiio
+                maxAge: 1000*60*60//la cookie tiene un tiempo de validez de 1 hora 
+            })
+            .send({user,token})
 
     }catch(error){
 
@@ -71,14 +117,41 @@ app.post('/registrar',async(req,res)=>
 app.post('/cerrar',(req,res)=>
 {
 
-    
+    res
+        .clearCookie('acceso_token')//se limpia la cookie para cerrar sesion
+        .json({message: 'Cerrado de sesion exitosoo!!'})
 
 })
-app.post('/protected',(req,res)=>
+//1-25-09
+app.get('/protected',(req,res)=>
 {
 
-    res.render('protected',{username:'roy'})
+    const token=req.cookies.acceso_token
+    if(!token)//si no tengo token
+    {
 
+        return res.status(403).send('Acceso no autorizado')//ruta protegida
+
+    }
+    try
+    {
+
+        const datos=jwt.verify(token,SECRET_JWT_KEY)//de este token va a extraer los datos, y estos datos vamos a tenerlos que 
+        if(typeof datos==='string')
+        {
+
+            return res.status(401).send('Token invalido')
+
+        }
+        //datos ya es jwtPayload (objeto)
+        return res.render('protected',{username: datos.username})//datos tendra el id del username y poder acceder a el y poder verlo en la pagina 
+
+    }catch(error){
+
+        res.status(401).send('Acceso no autorizado')
+
+    }
+    
 
 })
 
